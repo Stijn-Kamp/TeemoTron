a96225628ff114544599e68eada603a5bbcdf58b
Stijn Kamp <stijnkamp@hotmail.com>
2023-07-07 21:56:44 +0200

    semi working code

--- commands/playerinfo/opgg.js
+++ commands/playerinfo/opgg.js
@@ -1,10 +1,17 @@
-import { SlashCommandBuilder } from "discord.js";
+import discord from "discord.js";
+const { SlashCommandBuilder, EmbedBuilder } = discord;
+
+import { fileURLToPath } from "url";
 
 import selenium from "selenium-webdriver";
-const { Builder, By, Key, until } = selenium;
+
+const { Browser, Builder, By, Key, until } = selenium;
 
 async function getSummonerInfo(region, summonerName) {
-    const driver = await new Builder().forBrowser("chrome").build();
+    const driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .setChromeOptions(/* ... */)
+        .build();
     const url = `https://www.op.gg/summoners/${region}/${summonerName}`;
 
     try {
@@ -14,12 +21,98 @@ async function getSummonerInfo(region, summonerName) {
         // Wait for the page to load
         await driver.wait(until.elementLocated(By.className("content")), 5000);
 
-        // Scrape the data
-        const rankSolo = await driver
-            .findElement(By.css(".css-1v663t.e1x14w4w1 .tier"))
+        const contentHeader = await driver.findElement(By.id("content-header"));
+        const contentContainer = await driver.findElement(
+            By.id("content-container")
+        );
+
+        // Extract the data from the element
+        const profileName = await contentHeader
+            .findElement(By.className("summoner-name"))
             .getText();
+        const level = await contentHeader
+            .findElement(By.css(".level .level"))
+            .getText();
+        const previousTier = await contentHeader
+            .findElement(By.css(".prev-tier div"))
+            .getText();
+        const ladderRank = await contentHeader
+            .findElement(By.css(".ranking"))
+            .getText();
+        const lastUpdated = await contentHeader
+            .findElement(By.css(".last-update div"))
+            .getText();
+
+        const header = {
+            profileName,
+            level,
+            previousTier,
+            ladderRank,
+            lastUpdated,
+        };
+
+        const rankedSoloHeader = await driver
+            .findElement(By.css(".header"))
+            .getText();
+        const rankedSoloTier = await driver
+            .findElement(By.css(".tier"))
+            .getText();
+        const rankedSoloLP = await driver.findElement(By.css(".lp")).getText();
+        const rankedSoloWinLose = await driver
+            .findElement(By.css(".win-lose"))
+            .getText();
+        const rankedSoloWinRate = await driver
+            .findElement(By.css(".ratio"))
+            .getText();
+
+        const rankedFlexHeader = await driver
+            .findElement(By.css(".header"))
+            .getText();
+        const rankedFlexStatusElement = await driver.findElement(
+            By.css(".unranked")
+        );
+        const rankedFlexStatus = await rankedFlexStatusElement.getText();
+
+        const champions = [];
+        const championBoxes = await driver.findElements(
+            By.css(".champion-box")
+        );
+        for (const championBox of championBoxes) {
+            const championName = await championBox
+                .findElement(By.css(".name"))
+                .getText();
+            const championCS = await championBox
+                .findElement(By.css(".cs"))
+                .getText();
+            const championKDA = await championBox
+                .findElement(By.css(".kda .detail"))
+                .getText();
+            const championPlayed = await championBox
+                .findElement(By.css(".played .count"))
+                .getText();
+
+            champions.push({
+                name: championName,
+                cs: championCS,
+                kda: championKDA,
+                played: championPlayed,
+            });
+        }
+
+        const container = {
+            rankedSoloHeader,
+            rankedSoloTier,
+            rankedSoloLP,
+            rankedSoloWinLose,
+            rankedSoloWinRate,
+            rankedFlexHeader,
+            rankedFlexStatus,
+            champions,
+        };
+
         return {
-            rankSolo,
+            header,
+            container,
         };
     } catch (err) {
         console.error(err);
@@ -29,9 +122,64 @@ async function getSummonerInfo(region, summonerName) {
     }
 }
 
+function createProfileEmbed(profileData) {
+    // Extract profile information
+    const {
+        header: { profileName, level, previousTier, ladderRank, lastUpdated },
+        container: {
+            rankedSoloHeader,
+            rankedSoloTier,
+            rankedSoloLP,
+            rankedSoloWinLose,
+            rankedSoloWinRate,
+            rankedFlexHeader,
+            rankedFlexStatus,
+            champions,
+        },
+    } = profileData;
+
+    // Create a new embed
+    const embed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle("Player Profile")
+        .setURL("https://discord.js.org/")
+        .setAuthor({
+            name: profileName,
+            iconURL: "https://i.imgur.com/AfFp7pu.png",
+            url: "https://discord.js.org",
+        })
+        .setDescription("Some description here")
+        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+        .addFields(
+            { name: "Profile Name", value: profileName },
+            { name: "Level", value: level },
+            { name: "Previous Tier", value: previousTier },
+            { name: "Ladder Rank", value: ladderRank },
+            { name: "Last Updated", value: lastUpdated },
+            { name: "Ranked Solo", value: rankedSoloHeader },
+            { name: "Tier", value: rankedSoloTier },
+            { name: "LP", value: rankedSoloLP },
+            { name: "Win/Loss", value: rankedSoloWinLose },
+            { name: "Win Rate", value: rankedSoloWinRate },
+            { name: "Ranked Flex", value: rankedFlexHeader },
+            { name: "Status", value: rankedFlexStatus }
+        )
+        .setImage("https://i.imgur.com/AfFp7pu.png")
+        .setTimestamp()
+        .setFooter({
+            text: "Some footer text here",
+            iconURL: "https://i.imgur.com/AfFp7pu.png",
+        });
+
+    return embed;
+}
+
 async function main() {
-    const summonerInfo = await getSummonerInfo("euw", "Annénas");
-    console.log(summonerInfo);
+    getSummonerInfo("euw", "Mucho Bungo").then((summonerInfo) => {
+        console.log(summonerInfo);
+        const embedMessage = createProfileEmbed(summonerInfo);
+        console.log(embedMessage);
+    });
 }
 
 export const data = new SlashCommandBuilder()
@@ -39,8 +187,19 @@ export const data = new SlashCommandBuilder()
     .setDescription("Retrieves summoner info from op.gg");
 
 export const execute = async (interaction) => {
-    await interaction.reply(getSummonerInfo("euw", "Annénas"));
+    await interaction.deferReply();
+    getSummonerInfo("euw", "Annénas")
+        .then((summonerInfo) => {
+            if (summonerInfo) {
+                const embedMessage = createProfileEmbed(summonerInfo);
+                return interaction.editReply({ embeds: [embedMessage] });
+            }
+            return interaction.editReply("Summoner not found");
+        })
+        .catch((err) => {
+            return interaction.editReply("Error: " + err);
+        });
 };
 
 // only run the code if this file is being run directly
-if (import.meta.main) main();
+if (process.argv[1] === fileURLToPath(import.meta.url)) main();
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,75 +1,90 @@
+import discord from "discord.js";
+
 import fs from "node:fs";
 import path from "node:path";
-import discord from "discord.js";
-import dotenv from "dotenv";
 import { pathToFileURL } from "url";
+import { __dirname } from "./constants.js";
+
+import config from "./config.json" assert { type: "json" };
+import dotenv from "dotenv";
 
 dotenv.config();
-
-import { __dirname } from "./constants.js";
 
 const { REST, Routes } = discord;
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
-const guild = process.env.GUILD_ID;
+const guilds = config.guilds;
 
-const commands = [];
+async function getCommands(commandsFolder = "commands") {
+    const commands = [];
 
-// Grab all the command files from the commands directory you created earlier
-const commandsFolder = "commands";
-const commandsPath = path.join(__dirname, commandsFolder);
-const commandFolders = fs.readdirSync(commandsPath);
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(__dirname, commandsFolder);
+    const commandFolders = fs.readdirSync(commandsPath);
 
-for (const folder of commandFolders) {
-    const folderPath = path.join(commandsPath, folder);
-    const commandFiles = fs
-        .readdirSync(folderPath)
-        .filter((file) => file.endsWith(".js"));
+    for (const folder of commandFolders) {
+        const folderPath = path.join(commandsPath, folder);
+        const commandFiles = fs
+            .readdirSync(folderPath)
+            .filter((file) => file.endsWith(".js"));
 
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-    for (const file of commandFiles) {
-        const filePath = pathToFileURL(path.join(folderPath, file));
-        const command = await import(filePath);
+        // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+        for (const file of commandFiles) {
+            const filePath = pathToFileURL(path.join(folderPath, file));
+            const command = await import(filePath);
 
-        if ("data" in command && "execute" in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.log(
-                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-            );
+            if (command.data && command.execute) {
+                commands.push(command.data.toJSON());
+            } else {
+                console.log(
+                    `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+                );
+            }
         }
     }
+
+    return commands;
 }
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+function deployCommands(token, commands, clientId, guilds = []) {
+    // Construct and prepare an instance of the REST module
+    const rest = new REST().setToken(token);
 
-// Deploy the commands
-(async () => {
-    try {
-        console.log(
-            `Started refreshing ${commands.length} application (/) commands.`
-        );
-
-        // Use the put method to fully refresh all commands in the guild with the current set
-        rest.put(Routes.applicationGuildCommands(clientId, guild), {
-            body: commands,
-        }).then((data) => {
+    // Deploy the commands
+    (async () => {
+        try {
             console.log(
-                `Successfully reloaded ${data.length} guild (/) commands.`
+                `Started refreshing ${commands.length} application (/) commands.`
             );
-        });
 
-        // Use the put method to fully refresh all application commands
-        rest.put(Routes.applicationCommands(clientId), {
-            body: commands,
-        }).then((data) => {
-            console.log(
-                `Successfully reloaded ${data.length} application (/) commands.`
-            );
-        });
-    } catch (error) {
-        // Catch and log any errors
-        console.error(error);
-    }
-})();
+            // Use the put method to fully refresh all application commands
+            rest.put(Routes.applicationCommands(clientId), {
+                body: commands,
+            }).then((data) => {
+                console.log(
+                    `Successfully reloaded ${data.length} application (/) commands.`
+                );
+            });
+
+            // loop through each guild and deploy the commands
+            if (!Array.isArray(guilds)) return;
+            guilds.forEach((guildId) => {
+                // Use the put method to fully refresh all commands in the guild with the current set
+                rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+                    body: commands,
+                }).then((data) => {
+                    console.log(
+                        `Successfully reloaded ${data.length} guild (/) commands.`
+                    );
+                });
+            });
+        } catch (error) {
+            // Catch and log any errors
+            console.error(error);
+        }
+    })();
+}
+
+getCommands().then((commands) => {
+    deployCommands(token, commands, clientId, guilds);
+});
